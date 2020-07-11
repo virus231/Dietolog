c48365dc442c9a70d0944b9d255283013dcc00a9
Virus <vladprodan6@gmail.com>
2020-07-11 19:58:47 +0300

    +++

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,39 @@
 import 'bootstrap';
 
 import '../scss/index.scss';
-
-$('#alert').on('click', () => {
-  alert('jQuery works!');
-});
+import '../js/slick.min.js';
 
 // Your jQuery code
+
+$(document).ready(function(){
+  
+$('.center').slick({
+  centerMode: true,
+  // centerPadding: '60px',
+  slidesToShow: 1,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+    
+});
+
+
+
