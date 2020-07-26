d525f7b100be70a4a5f3fad7fb8ef53ecd8485cf
Virus <vladprodan6@gmail.com>
2020-07-26 12:20:26 +0300

    formik

--- src/js/index.js
+++ src/js/index.js
@@ -9,11 +9,11 @@ import '../js/slick.min.js';
 // });
 
 $(document).ready(function() {
-  $('#container1').twentytwenty();
+  // $('#container1').twentytwenty();
 
   $('.center').slick({
     centerMode: true,
-    centerPadding: '60px',
+    centerPadding: '0px',
     slidesToShow: 3,
     responsive: [
       {
@@ -50,7 +50,7 @@ $(document).ready(function() {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToScroll: 2,
           infinite: true,
           dots: true,
         },
@@ -59,7 +59,7 @@ $(document).ready(function() {
         breakpoint: 600,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2,
+          slidesToScroll: 1,
         },
       },
       {
